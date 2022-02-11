07eff442106d55d6bc583b2ad44e76b8194ce59b
Nouhayla Erraou <nerraou@e1r8p5.1337.ma>
2022-02-11 18:58:23 +0100

    draw the last line

--- draw/draw_lines.c
+++ draw/draw_lines.c
@@ -6,7 +6,7 @@
 /*   By: nerraou <nerraou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 17:35:32 by nerraou           #+#    #+#             */
-/*   Updated: 2022/02/11 17:36:01 by nerraou          ###   ########.fr       */
+/*   Updated: 2022/02/11 18:55:17 by nerraou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,22 @@ void draw_lines(t_point ***tab, t_data *data, int w, int h)
 		while (j < w)
 		{
 			p1 = iso(tab[i][j]->x, tab[i][j]->y, tab[i][j]->z);
-			if (j + 1 >= w)
-				break ;
-			p2 = iso(tab[i][j+1]->x, tab[i][j+1]->y, tab[i][j+1]->z);
-			draw_line(data , p1 ,p2 ,tab[i][j]->color);
-			if (i + 1 >= h)
-				break ;
-			p2 = iso(tab[i+1][j]->x, tab[i+1][j]->y, tab[i + 1][j]->z);
-			draw_line(data , p1 ,p2 ,tab[i][j]->color);
+			// if (j + 1 >= w)
+			// 	break ;
+			if(j < w -1)
+			{
+				p2 = iso(tab[i][j+1]->x, tab[i][j+1]->y, tab[i][j+1]->z);
+				draw_line(data , p1 ,p2 ,tab[i][j]->color);
+			}
+			
+			// if (i + 1 >= h)
+			// 	break ;
+			if (i < h -1)
+			{
+				p2 = iso(tab[i+1][j]->x, tab[i+1][j]->y, tab[i + 1][j]->z);
+				draw_line(data , p1 ,p2 ,tab[i][j]->color);
+			}
+			
 			j++;
 		}
 		i++;
